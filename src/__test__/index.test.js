f7cae484319a138985f0a48b5c132c781d6bc960
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-08-03 11:53:17 +0200

    Initial commit, all setup is done

---
+++ src/__test__/index.test.js
@@ -0,0 +1,4 @@
+const { add } = require('../index.js');
+test('testing', () => {
+  expect(add(2, 2)).toBe(4);
+});
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(scss|sass)$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|webp|avif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              context: 'public',
+              name: 'images/[name].[hash:8].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Battleship',
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+    hot: true,
+  },
+};
